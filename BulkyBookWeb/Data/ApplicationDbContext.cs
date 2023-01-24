d899e761022073d7594986304eb9602848d82dcc
davide casati <davide.casati@issgreppi.it>
2023-01-24 12:34:06 +0100

    tutto

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BulkyBookWeb.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+
+{
+
+    public class ApplicationDbContext : DbContext
+
+    {
+
+        //ApplicationDbContext riceverà nel costruttore le opzioni per potersi connettere al database
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        {
+
+        }
+
+        //la property Categories permetterà di creare la tabella Categories in fase di Migration e di poter accedere ai dati in
+
+        //essa contenuti
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+    }
+
+}
