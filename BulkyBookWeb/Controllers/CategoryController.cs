28cc59a24ecf4eb5ddcb7710a38bb4b0317dc0e2
davide casati <davide.casati@issgreppi.it>
2023-01-25 09:10:09 +0100

    add controller e view Category

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            // recupero info da db
+            IEnumerable<Category> categoryListObject = _db.Categories;
+            return View(categoryListObject);
+        }
+    }
+}
