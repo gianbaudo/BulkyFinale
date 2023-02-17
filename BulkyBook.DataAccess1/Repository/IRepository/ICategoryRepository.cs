9b8686eb5ef936a39ea39104f57816e6272e555f
davide casati <davide.casati@issgreppi.it>
2023-02-17 09:52:46 +0100

    +

--- BulkyBook.DataAccess1/Repository/IRepository/ICategoryRepository.cs
+++ BulkyBook.DataAccess1/Repository/IRepository/ICategoryRepository.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using BulkyBook.Models1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static BulkyBook.DataAccess1.Repository.IRepository.IRepository;
 
 namespace BulkyBook.DataAccess1.Repository.IRepository
 {
-	internal class ICategoryRepository
+	public interface ICategoryRepository : IRepository<Category>
 	{
+		void Update(Category category);
+		void Save();
 	}
+
 }
