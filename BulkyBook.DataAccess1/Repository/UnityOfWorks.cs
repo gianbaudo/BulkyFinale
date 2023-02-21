39c2830e9eff4576b47c484f5104c51c89e3561b
davide casati <davide.casati@issgreppi.it>
2023-02-21 09:35:16 +0100

    Add UnityOfWork

---
+++ BulkyBook.DataAccess1/Repository/UnityOfWorks.cs
@@ -0,0 +1,27 @@
+﻿using BulkyBook.DataAccess1.Repository.IRepository;
+using BulkyBookWeb.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess1.Repository
+{
+	public class UnityOfWorks : IUnityOfWork
+	{
+		private readonly ApplicationDbContext _db;
+		public UnityOfWorks(ApplicationDbContext db)
+		{
+			_db = db;
+			Category = new CategoryRepository(_db);
+		}
+		public ICategoryRepository Category { get; private set; } = null!;
+
+		public void Save()
+		{
+			_db.SaveChanges();
+		}
+
+	}
+}
