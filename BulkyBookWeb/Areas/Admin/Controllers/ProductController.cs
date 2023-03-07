548e2413535c2cb0059a5a8278ab69932b56d176
davide casati <davide.casati@issgreppi.it>
2023-03-07 10:58:18 +0100

    Upsert

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -30,65 +30,42 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return View(productListObject);
         }
 
-        //get Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken] //per evitare che l'utente crei dall'url
-        public IActionResult Create(Product product)
-        {
-            //if (obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError(nameof(obj.Name), $"The name of property {nameof(obj.DisplayOrder)} cannot exactly match the name of property {nameof(obj.Name)}");
-            //}
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Add(product);
-                _unitOfWork.Save();
-                TempData["success"] = "Product created successfully";
-                return RedirectToAction("Index");
-            }
-            return View(product);
-        }
-
         //EDIT
-        public IActionResult Edit(int? id)
+        public IActionResult Upsert(int? id)
         {
+            Product product = new();
+
             if (id == null || id == 0)
             {
-                return NotFound();
+                //create product
+                return View(product);
             }
-            var productFromDbFirst = _unitOfWork.Product.GetFirstOrDefault(c => c.Id == id);
-            if (productFromDbFirst == null)
+            else
             {
-                return NotFound();
+                //update product
             }
-            return View(productFromDbFirst);
+            return View(product);
+
 
         }
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Edit(Product product)
-		{
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
 
-			if (ModelState.IsValid)
-			{
-				_unitOfWork.Product.Update(product);
-				_unitOfWork.Save();
-				TempData["success"] = "Product updated successfully";
-				return RedirectToAction(nameof(Index));
-			}
-			return View(product);
-
-		}
+        }
 
 
-		//GET
-		public IActionResult Delete(int? id)
+        //GET
+        public IActionResult Delete(int? id)
 		{
 			if (id == null || id == 0)
 			{
